35f15d07a9a1e138c327dd5a400ac7ddaf9b5364
Juan Pablo <j.tobares@alumno.um.edu.ar>
2018-09-06 16:06:02 -0300

    Trabajo Practico n°1

--- Tp1/ejercicio4.php
+++ Tp1/ejercicio4.php
@@ -5,11 +5,17 @@
 </head>
 <body>
 	<?php
+		echo "<pre>";
 		$st = "1,2,3,4,5";
-		echo "String: $st";
-		foreach ($st as $arr) {
-			echo "Arreglo $arr";
-		}
+		$arr2 = array('0' => 1, '1'=>2,'2'=>3,'3'=>4,'4'=>5 );
+		echo "String 1: $st <br><br> Arreglo 1:";
+		$arr1 = str_split($st);
+		print_r($arr1);
+		echo "<br><br>Arreglo 2:";
+		print_r($arr2);
+		$st1 = implode(",", $arr2);
+		echo "String 2: $st1";
+		echo "</pre>";
 	?>
 </body>
 </html>
