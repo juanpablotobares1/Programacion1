923fb9aadfe4bd47d25b989c9d6a9980e5175b40
root <root@j.tobares>
2018-09-12 15:46:31 -0300

    Trabajo Práctico N°2

---
+++ Tp2/ejercicio1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ejercicio 1</title>
+</head>
+<body>
+	<?php
+		echo "Hola $_GET[nombre]. <br>Tu Email es $_GET[email]";  
+	?>
+</body>
+</html>
---
+++ Tp2/ejercicio2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Ejercicio 2</title>
+</head>
+<body>
+	<?php
+		echo "Hola $_POST[nombre]. <br>Tu Email es $_POST[email]";  
+	?>
+</body>
+</html>
