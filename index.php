af539f3b21c89e9ca0f3b1440178bee43a65edd8
root <root@j.tobares>
2018-09-12 06:20:06 -0300

    Inicio

---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+<style></style>
+    <title>Trabajo Practico 1</title>
+    <link rel="stylesheet" type="text/css" href="index.css">
+</head>
+<body background="fondo.jpeg">
+    <center>
+        <h1 class="tit">PROGRAMACION 1</h1>
+        <table border="6" bgcolor="white">
+
+        <tr >
+            <td bgcolor="red">
+                <b>Trabajo Practico 1</b>
+            </td>
+            <td>
+                <a href="/Programacion1/Tp1/ejercicio1.php"><h4>   Ejercicio 1</h4></a>
+
+            </td>
+            <td>
+                 <a href="/Programacion1/Tp1/ejercicio2.php"><h4>   Ejercicio 2</h4></a>
+            </td>
+            <td>
+                 <a href="/Programacion1/Tp1/ejercicio3.php"><h4>   Ejercicio 3</h4></a>
+            </td>
+            <td>
+                <a href="/Tp1/ejercicio4.php"><h4>   Ejercicio 4</h4></a>
+            </td>
+            <td>
+                <a href="/Programacion1/Tp1/ejercicio5.php"><h4>   Ejercicio 5</h4></a>
+            </td>
+            <td>
+                <a href="/Programacion1/Tp1/ejercicio7.php"><h4>   Ejercicio 7</h4></a>
+            </td>
+            <td>
+                 <a href="/Programacion1/Tp1/ejercicio8.php"><h4>   Ejercicio 8</h4></a>
+            </td>
+            <td>
+                <a href="/Programacion1/Tp1/ejercicio9.php"><h4>   Ejercicio 9</h4></a>
+            </td>
+            <td>
+                <a href="/Programacion1/Tp1/ejercicio10.php"><h4>   Ejercicio 10</h4></a>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <a href="/Programacion1/Tp1/ejercicio11.php"><h4>   Ejercicio 11</h4></a>
+            </td>
+            <td>
+                <a href="/Programacion1/Tp1/ejercicio12.php"><h4>   Ejercicio 12</h4></a>
+            </td>
+        </tr>
+    </table><br><br>
+    <table border="6" bgcolor="white">
+        <tr>
+            <td bgcolor="blue">
+                <b>Trabajo Practico 1</b>
+            </td>
+            <td>
+                <a href="/Programacion1/Tp2/ejercicio1.php"><h4>   Ejercicio 1</h4></a>
+
+            </td>
+            <td>
+                 <a href="/Programacion1/Tp2/ejercicio2.php"><h4>   Ejercicio 2</h4></a>
+            </td>
+            <td>
+                 <a href="/Programacion1/Tp2/ejercicio3.php"><h4>   Ejercicio 3</h4></a>
+            </td>
+            <td>
+                <a href="/Programacion1/Tp2/ejercicio4.php"><h4>   Ejercicio 4</h4></a>
+            </td>
+            <td>
+                <a href="/Programacion1/Tp2/ejercicio5.php"><h4>   Ejercicio 5</h4></a>
+            </td>
+            <td>
+                <a href="/Programacion1/Tp2/ejercicio7.php"><h4>   Ejercicio 7</h4></a>
+            </td>
+            <td>
+                 <a href="/Programacion1/Tp2/ejercicio8.php"><h4>   Ejercicio 8</h4></a>
+            </td>
+            <td>
+                <a href="/Programacion1/Tp2/ejercicio9.php"><h4>   Ejercicio 9</h4></a>
+            </td>
+            <td>
+                <a href="/Programacion1/Tp2/ejercicio10.php"><h4>   Ejercicio 10</h4></a>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <a href="/Programacion1/Tp2/ejercicio11.php"><h4>   Ejercicio 11</h4></a>
+            </td>
+            <td>
+                <a href="/Programacion1/Tp2/ejercicio12.php"><h4>   Ejercicio 12</h4></a>
+            </td>
+        </tr>
+
+        </table>
+    </center>
+
+</body>
+</html>
