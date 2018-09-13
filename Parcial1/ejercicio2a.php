62ae4fd4a44e7d2b5951bfd0f7663683f75730da
root <root@j.tobares>
2018-09-13 10:31:11 -0300

    Primer Parcial

--- Parcial1/ejercicio2a.php
+++ Parcial1/ejercicio2a.php
@@ -6,6 +6,7 @@
 <body>
 <form action="ejercicio2b.php" method="get">
     <?php
+    //DIA
     echo "Hoy es: ".date("j-F-Y")."<br><br>";
     echo "Dia: <select name='dia'>";
     for ($i=1;$i < 32; $i++) {
@@ -14,111 +15,33 @@
         }else
             echo "<option value=".$i.">".$i."</option>";
     }
-    ?>
-    </select>
-    <br>
-    <br>
-    <?php
-    echo "Mes: "."<select name='mes'>";
-    for ($i=1;$i < 2; $i++)  {
-        switch ($b) {
-            case 1:
-                $mes = "Enero";
-                if (1==date(n)) {
-                    echo "<option selected='true' value=".$mes.">".$mes."</option>";
-                }else
-                    echo "<option value=".$mes.">".$mes."</option>";
-            case 2:
-                $mes = "Febrero";
-                if (2==date(n)) {
-                    echo "<option selected='true' value=".$mes.">".$mes."</option>";
-                }else
-                    echo "<option value=".$mes.">".$mes."</option>";
-            case 3:
-                $mes = "Marzo";
-                if (3==date(n)) {
-                    echo "<option selected='true' value=".$mes.">".$mes."</option>";
-                }else
-                    echo "<option value=".$mes.">".$mes."</option>";
-            case 4:
-                $mes = "Abril";
-                if (4==date(n)) {
-                    echo "<option selected='true' value=".$mes.">".$mes."</option>";
-                }else
-                    echo "<option value=".$mes.">".$mes."</option>";
-            case 5:
-                $mes = "Mayo";
-                if (5==date(n)) {
-                    echo "<option selected='true' value=".$mes.">".$mes."</option>";
-                }else
-                    echo "<option value=".$mes.">".$mes."</option>";
-            case 6:
-                $mes = "Junio";
-                if (5==date(n)) {
-                    echo "<option selected='true' value=".$mes.">".$mes."</option>";
-                }else
-                    echo "<option value=".$mes.">".$mes."</option>";
-            case 7:
-                $mes = "Julio";
-                if (5==date(n)) {
-                    echo "<option selected='true' value=".$mes.">".$mes."</option>";
-                }else
-                    echo "<option value=".$mes.">".$mes."</option>";
-            case 8:
-                $mes = "Agosto";
-                if (5==date(n)) {
-                    echo "<option selected='true' value=".$mes.">".$mes."</option>";
-                }else
-                    echo "<option value=".$mes.">".$mes."</option>";
-            case 9:
-                $mes = "Septiembre";
-                if (5==date(n)) {
-                    echo "<option selected='true' value=".$mes.">".$mes."</option>";
-                }else
-                    echo "<option value=".$mes.">".$mes."</option>";
-            case 10:
-                $mes = "Octubre";
-                if (5==date(n)) {
-                    echo "<option selected='true' value=".$mes.">".$mes."</option>";
-                }else
-                    echo "<option value=".$mes.">".$mes."</option>";
-            case 11:
-                $mes = "Noviembre";
-                if (5==date(n)) {
-                    echo "<option selected='true' value=".$mes.">".$mes."</option>";
-                }else
-                    echo "<option value=".$mes.">".$mes."</option>";
-            case 12:
-                $mes = "Diciembre";
-                if (5==date(n)) {
-                    echo "<option selected='true' value=".$mes.">".$mes."</option>";
-                }else
-                    echo "<option value=".$mes.">".$mes."</option>";
-        }
-    }
-    ?>
-    </select>
-    <br>
-    <br>
-    <?php
-    echo "Año: "."<select name='ano'>";
-    for ($c=1900;$c < 2101; $c++){
-        if ($c==date(Y)) {
-            echo "<option selected='true' value=" . $c . ">" . $c . "</option>";
+    echo "</select><br><br>";
+    //MES
+    echo "Mes: <select type='text' name='mes'>";
+    $mes = array('1' => "Enero", '2'=>"Febrero",'3'=>"Marzo",'4'=>"Abril",'5'=>"Mayo",'6'=> "Junio",'7'=>"Julio",'8'=>"Agosto",'9'=>"Septiembre",'10'=>"Octubre",'11'=>"Noviembre",'12'=>"Diciembre" );
+    for ($i=1;$i < 13; $i++) {
+        if ($i==date(n)) {
+            echo "<option selected='true' value=".$mes[$i].">".$mes[$i]."</option>";
         }else
-            echo "<option value=".$c.">".$c."</option>";
+            echo "<option value=".$mes[$i].">".$mes[$i]."</option>";
     }
-
-    echo "
-    <label for='checkbox'>Mostrar formato corto:</label>
-    ";
-
-
+    echo "</select><br><br>";
+    //AÑO
+    echo "Año: "."<select name='ano'>";
+    for ($i=1900;$i < 2101; $i++){
+        if ($i==date(Y)) {
+            echo "<option selected='true' value=".$i.">".$i."</option>";
+        }else
+            echo "<option value=".$i.">".$i."</option>";
+    }
     ?>
-    </select>
-    <br>
-    <br>
-    <input type="submit">
+    </select><br><br>
+    <label for='checkbox'>Mostrar formato corto:</label>
+    
+    Si <input type="radio" name="corto" value="1">
+    No <input type="radio" name="largo" value="2"><br><br>
+    <input type="submit" value="Enviar">
+    <input type="reset" value="Borrar">
 </form>
 </body>
 </html>
--- Parcial1/ejercicio2b.php
+++ Parcial1/ejercicio2b.php
@@ -5,7 +5,42 @@
 </head>
 <body>
 	<?php
-		echo "Hoy es ".$_GET['dia']." de ".$_GET['mes']." de ".$_GET['ano'];
+	if (isset($_GET['corto'])) {
+		echo "Hoy es ".$_GET['dia']."/".$_GET['mes']."/".$_GET['ano'];
+	} else {
+		if (isset($_GET['largo'])) {
+			echo "Hoy es ".$_GET['dia']." de ".$_GET['mes']." de ".$_GET['ano'];
+		} else {
+			echo "No eligio el tipo de formato para mostrar la fecha";
+		}
+		
+	}
+	echo "<br><br>Dia: <select name='dia'>";
+    for ($i=1;$i < 32; $i++) {
+        if ($i==$_GET['dia']) {
+            echo "<option selected='true' value=".$i.">".$i."</option>";
+        }else
+            echo "<option value=".$i.">".$i."</option>";
+    }
+    echo "</select><br><br>";
+    //MES
+    echo "Mes: <select type='text' name='mes'>";
+    $mes = array('1' => "Enero", '2'=>"Febrero",'3'=>"Marzo",'4'=>"Abril",'5'=>"Mayo",'6'=> "Junio",'7'=>"Julio",'8'=>"Agosto",'9'=>"Septiembre",'10'=>"Octubre",'11'=>"Noviembre",'12'=>"Diciembre" );
+    for ($i=1;$i < 13; $i++) {
+        if ($i==$_GET['mes']) {
+            echo "<option selected='true' value=".$mes[$i].">".$mes[$i]."</option>";
+        }else
+            echo "<option value=".$mes[$i].">".$mes[$i]."</option>";
+    }
+    echo "</select><br><br>";
+    //AÑO
+    echo "Año: "."<select name='ano'>";
+    for ($i=1900;$i < 2101; $i++){
+        if ($i==$_GET['ano']) {
+            echo "<option selected='true' value=".$i.">".$i."</option>";
+        }else
+            echo "<option value=".$i.">".$i."</option>";
+    }
 	?>
 </body>
 </html>
