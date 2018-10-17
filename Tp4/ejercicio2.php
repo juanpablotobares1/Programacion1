c66c96460c7018b4d20ac6a142a2c789ef375287
juanpablotobares1 <juanpablotobares1@gmail.com>
2018-10-17 16:50:03 -0300

    Trabajo Práctico n°4

--- Tp4/ejercicio2.php
+++ Tp4/ejercicio2.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
+	<meta charset="utf-8">
 	<title>Ejercicio 2</title>
 </head>
 <body>
@@ -8,18 +9,29 @@
 <h3>Tabla: persona</h3>
 <p>Listado de datos:</p>
 
+<table border="1">
 <?php  
 $servidor = 'localhost';
 $usuario = 'juanpablo';
 $clave = '9886398319';
 $database = 'programacion1';
 
-$conexion = new PDO("mysql: host = $servidor; dbname = programacion1", $usuario, $clave);
+$conexion = new PDO("mysql: host=$servidor; dbname=$database", $usuario, $clave);
 
 $sql = 'select * from persona';
+$ejecsql = $conexion -> prepare($sql);
+$ejecsql -> execute();
 
-
+while ($fila = $ejecsql -> fetch(PDO::FETCH_ASSOC)) {
+	echo "<tr>";
+	foreach ($fila as $key) {
+		echo "<td>";
+		echo $key;
+		echo "</td>";
+	}
+	echo "</tr>";
+}
 ?>
-
+</table>
 </body>
 </html>
